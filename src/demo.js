4c452f9432bea5d459f9a18f4234b4362e1792c3
BStorm-Phil <philippe.haerens@bstorm.be>
2025-02-27 16:31:38 +0100

    projet poke api - json server

--- src/demo.js
+++ src/demo.js
@@ -3,42 +3,52 @@ const axios = require('axios');
 const main = async () => {
     console.log("Debut du programme.")
 
-    let id = 10;
+    let next = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=200";
+    do {
+        const responseList = await axios.get(next);
+        next = responseList.data.next;
 
-    let pokemon = null;
-    try {
-        // regarder si je ne possede déjà pas le pokemon avec l'ID dans mon json-server
-        const response = await axios.get("http://localhost:3000/pokemons/" + id);
-        pokemon = response.data;
-    } catch (e) {
-        // ne rien faire
-        console.log(e.status);
-    }
+        const pokemonTab = responseList.data.results;
 
-    if (!pokemon) {
-        // chercher dans la poke api
-        let pokemonInsert = null;
-        try {
-            const response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + id);
-            pokemonInsert = response.data;
-        } catch (e) {
-            console.log("Il y a un probleme avec la pokepai");
-            return;
+        for (const poke of pokemonTab) {
+            const id = poke.url.split("/")[6];
+
+            let pokemon = null;
+            try {
+                // regarder si je ne possede déjà pas le pokemon avec l'ID dans mon json-server
+                const response = await axios.get("http://localhost:3000/pokemons/" + id);
+                pokemon = response.data;
+            } catch (e) {
+                // ne rien faire
+                console.log(e.status);
+            }
+
+            if (!pokemon) {
+                // chercher dans la poke api
+                let pokemonInsert = null;
+                try {
+                    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + id);
+                    pokemonInsert = response.data;
+                } catch (e) {
+                    console.log("Il y a un probleme avec la pokepai");
+                    return;
+                }
+
+                try {
+                    await axios.post("http://localhost:3000/pokemons", {
+                        id: pokemonInsert.id,
+                        name: pokemonInsert.name
+                    })
+                } catch (e) {
+                    console.log(e.status);
+                    return;
+                }
+            } else {
+                console.log("exist déja", pokemon);
+            }
         }
 
-        try {
-            await axios.post("http://localhost:3000/pokemons", {
-                id: pokemonInsert.id,
-                name: pokemonInsert.name
-            })
-        } catch (e) {
-            console.log(e.status);
-            return;
-        }
-    } else {
-        console.log("exist déja", pokemon);
-    }
-
+    } while (next);
     console.log("Fin du programme");
 };
 
